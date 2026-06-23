d522c6b7f87a6101b0c92624d459faf9b99732dd
Jungyh0 <yh990920@naver.com>
2026-06-23 14:54:58 +0900

    Fix appspec permissions and start script

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-JAR_PATH=$(ls /home/ec2-user/app/bookapp/build/libs/*.jar | tail -1)
+JAR_PATH=$(ls /home/ec2-user/app/bookapp/build/libs/*.jar | grep -v plain | tail -1)
 pkill -f 'java -jar' || true
 sleep 3
 nohup java -jar $JAR_PATH > /home/ec2-user/app/app.log 2>&1 &
