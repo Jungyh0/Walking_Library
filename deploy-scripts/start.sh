55bf684f285a63e8425ba812b84d9bd4f11fcb17
Jungyh0 <yh990920@naver.com>
2026-06-23 14:36:24 +0900

    Add appspec and deploy scripts

---
+++ deploy-scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+JAR_PATH=$(ls /home/ec2-user/app/bookapp/build/libs/*.jar | tail -1)
+pkill -f 'java -jar' || true
+sleep 3
+nohup java -jar $JAR_PATH > /home/ec2-user/app/app.log 2>&1 &
