2fb4784479b1b8f80fba138b675e2ef67511bf6e
Jungyh0 <yh990920@naver.com>
2026-06-24 15:11:53 +0900

    add pakage

--- bookapp/src/main/java/com/aivle/bookapp/HealthCheckController.java
+++ bookapp/src/main/java/com/aivle/bookapp/HealthCheckController.java
@@ -1,3 +1,5 @@
+package com.aivle.bookapp;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
